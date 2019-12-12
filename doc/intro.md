cc095d03b881752287ab3a4ebeb4a73999d2233f
Paula Gearon <pgearon@cisco.com>
2019-12-11 22:26:21 -0500

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to asami-loom
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/asami_loom/core_test.clj
@@ -0,0 +1,7 @@
+(ns asami-loom.core-test
+  (:require [clojure.test :refer :all]
+            [asami-loom.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
